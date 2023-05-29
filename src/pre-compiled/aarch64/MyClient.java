1087a75879d57c63879ea0f18a2555d3f826693f
sasha20022002 <80363788+sasha20022002@users.noreply.github.com>
2023-05-29 19:10:30 +1000

    implemented helper functions to write less

--- src/pre-compiled/aarch64/MyClient.java
+++ src/pre-compiled/aarch64/MyClient.java
@@ -2,10 +2,7 @@ import java.net.*;
 import java.nio.Buffer;
 import java.io.*;
 
-
 public class MyClient {
-
-
     /**
      * @param args
      * @throws Exception
@@ -18,37 +15,25 @@ public class MyClient {
             int largestServerCores = 0;
             int largestServerID = 0;
 
-
             // Connection to the server
             Socket socket = new Socket("127.0.0.1", 50000);
-
 
             // Setup the input and output streams
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 
-
             // Read server response and store it in a variable
-            String serverResponse = input.readLine();
-
+            String serverResponse = Receive(input);
 
             // Send the HELO message and the auth message
-            output.writeBytes("HELO\n"); // Send HELO
-            output.flush();
-
+            Send(output, "HELO"); // Send HELO
 
             // Send "AUTH" to server to identify user
-            output.write(("AUTH\n").getBytes());
-            output.flush();
-            serverResponse = input.readLine(); // Receive OK
+            Send(output, "AUTH");
+            serverResponse = Receive(input); // Receive OK
 
-
-            output.write(("REDY\n").getBytes());
-            output.flush();
-
-
-            serverResponse = input.readLine();
-
+            Send(output, "REDY");
+            serverResponse = Receive(input);
 
             String[] jobInfo = serverResponse.split(" ");
             int jobValue = Integer.parseInt(jobInfo[2]);
@@ -56,47 +41,36 @@ public class MyClient {
             int jobMemory = Integer.parseInt(jobInfo[5]);
             int jobDisk = Integer.parseInt(jobInfo[6]);
 
-
             System.out.println("INFO: " + " Number: " +  jobValue + " " + " JobCores: " + jobCores + " " + " jobMemory: " + jobMemory + " " + " jobDisk: " + jobDisk);
-
 
             String[] serverList = serverResponse.split(" ");
 
             int serverNums = Integer.parseInt(serverList[1]);
 
-            output.write(("GETS Avail " + jobCores + " " + jobMemory + " " + jobDisk + "\n").getBytes());
-            output.flush();
-            serverResponse = input.readLine();
-
+            Send(output, "GETS Avail " + jobCores + " " + jobMemory + " " + jobDisk);
+            serverResponse = Receive(input);
 
             if (!serverResponse.startsWith("OK")) {
-                output.write(("GETS Capable " + jobCores + " " + jobMemory + " " + jobDisk + "\n").getBytes());
-                output.flush();
-                serverResponse = input.readLine();
+                Send(output, "GETS Capable " + jobCores + " " + jobMemory + " " + jobDisk);
+                serverResponse = Receive(input);
             }
 
-
-            output.write(("OK\n").getBytes());
-            output.flush();
-
+            Send(output, "OK");
 
             int serverCount = Integer.parseInt(serverList[1]);
             int[] serverCores = new int[serverCount];
             int[] serverID = new int[serverCount];
             String[] serverType = new String[serverCount];
 
-
             // Number of servers that we are allocating jobs to
             int i = 0;
             while (i < serverNums) {
-                serverResponse = input.readLine();
+                serverResponse = Receive(input);
                 String[] serverData = serverResponse.split(" ");
-
 
                 serverID[i] = Integer.parseInt(serverData[1]);
                 serverCores[i] = Integer.parseInt(serverData[4]);
                 serverType[i] = serverData[0];
-
 
                 if (isFlag && serverCores[i] > largestServerCores) {
                     largestServerType = serverType[i];
@@ -104,21 +78,15 @@ public class MyClient {
                     largestServerID = serverID[i];
                     isFlag = false;
                 }
-
-
                 i++;
             }
 
-
-            output.write(("OK\n").getBytes());
-            output.flush();
-
+            Send(output, "OK");
 
             // Implementation of bin packing using First Fit algorithm
             boolean isJobAllocated = false;
             int bestFitIndex = -1;
             int bestFitRemaining = Integer.MAX_VALUE;
-
 
             for (int j = 0; j < serverCount; j++) {
                 if (serverCores[j] >= jobCores) {
@@ -130,47 +98,38 @@ public class MyClient {
                 }
             }
 
-
             if (bestFitIndex != -1) {
-                output.write(("SCHD " + jobValue + " " + serverType[bestFitIndex] + " " + serverID[bestFitIndex] + "\n").getBytes());
-                output.flush();
-                serverResponse = input.readLine();
-
+                Send(output, "SCHD " + jobValue + " " + serverType[bestFitIndex] + " " + serverID[bestFitIndex]);
+                serverResponse = Receive(input);
 
                 if (serverResponse.equals("OK")) {
                     isJobAllocated = true;
                 }
             }
 
-
             // Schedule to the largest server with most cores if no suitable server found
             if (!isJobAllocated) {
-                output.write(("SCHD " + jobValue + " " + largestServerType + " " + largestServerID + "\n").getBytes());
-                output.flush();
-                serverResponse = input.readLine();
+                Send(output, "SCHD " + jobValue + " " + largestServerType + " " + largestServerID);
+                serverResponse = Receive(input);
             }
-
 
             socket.close();
         } catch (Exception exception) {
             System.out.println(exception);
         }
     }
-
     public static String Send(DataOutputStream output, String s) throws IOException{
         output.write(("" + s + "\n").getBytes());
         output.flush();
         return s;
     }
 
-    public static String Recieve(BufferedReader input) throws IOException{
+    public static String Receive(BufferedReader input) throws IOException{
         String mess = input.readLine(); 
         return mess; 
     }
+
+
+
+
 }
-
-
-
-
-
-
